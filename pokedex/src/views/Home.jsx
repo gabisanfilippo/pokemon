4a2d5558d41b88955c520568a921b8fe68235c3b
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2022-06-19 21:48:38 -0300

    feat: creating the Cards component

--- pokedex/src/views/Home.jsx
+++ pokedex/src/views/Home.jsx
@@ -1,10 +1,28 @@
+import { PokemonContext } from "../contexts/Pokemons"
+import { useContext } from "react"
+import Cards from "../components/Cards"
+import styled from "styled-components"
 
+const Container = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+    justify-content: center;
+`
 
 function Home(){
+    const {pokemon} = useContext(PokemonContext)
     return(
-        <>
-            <p>oi</p>
-        </>
+        <Container>
+            {pokemon?.map((index)=>{
+                return(
+                    <Cards 
+                        key={index.name}
+                        name={index.name}
+                    />
+                )
+            })}
+        </Container> 
     )
 }
 export default Home
